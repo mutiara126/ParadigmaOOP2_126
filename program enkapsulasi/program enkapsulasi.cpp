69ca2a1cdfe9dad283ea2e4b9fb97de3b486eaf8
mutiara126 <salsamutiara1812@gmail.com>
2023-05-29 14:13:34 +0800

    untuk mendeklarasikan remote lampu, lampu rumah

--- program enkapsulasi/program enkapsulasi.cpp
+++ program enkapsulasi/program enkapsulasi.cpp
@@ -13,3 +13,20 @@ public:
 	}
 };
 
+int main() {
+	remoteLampu lampuRumah;
+
+	lampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
+	lampuRumah.setSaklarNo(1, "Lampu Rumah Tamu");
+	lampuRumah.setSaklarNo(2, "Lampu Kamar Tidur");
+	lampuRumah.setSaklarNo(3, "Lampu Dapur");
+
+	cout << lampuRumah.getSaklarNo(0) << endl;
+	cout << lampuRumah.getSaklarNo(1) << endl;
+	cout << lampuRumah.getSaklarNo(2) << endl;
+	cout << lampuRumah.getSaklarNo(3) << endl;
+
+	
+
+
+}
